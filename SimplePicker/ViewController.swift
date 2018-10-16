8eea98f308d4901833a6049cd8a7e424be0ed89e
Huy Tran <hoang.huy.tran@gmail.com>
2018-10-16 17:58:59 +1100

    Initial Commit

---
+++ SimplePicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimplePicker
+//
+//  Created by Huy Tran on 16/10/18.
+//  Copyright © 2018 Huy Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
