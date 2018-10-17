34d66dcdc9fb616fa7f5cf2fc865d0a2542e170f
Huy Tran <hoang.huy.tran@gmail.com>
2018-10-17 16:48:09 +1100

    Add README and working code

---
+++ README.md
@@ -0,0 +1,9 @@
+[![MIT license](https://img.shields.io/badge/License-MIT-blue.svg?style=flat)](https://github.com/htr3n/SimplePicker/blob/master/LICENSE) ![GitHub forks](https://img.shields.io/github/forks/htr3n/SimplePicker.svg) ![GitHub issues](https://img.shields.io/github/issues/htr3n/SimplePicker.svg) ![GitHub issues closed](https://img.shields.io/github/issues-closed/htr3n/SimplePicker.svg)
+
+
+# Simple Demo of UIPickerView
+
+This project demonstrates a simple usage of [UIPickerView](https://developer.apple.com/documentation/uikit/uipickerview) with one-dimensional data source in Swift 4 and Xcode 9+.
+
+
+
--- SimplePicker/ViewController.swift
+++ SimplePicker/ViewController.swift
@@ -8,13 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var picker: UIPickerView!
+    @IBOutlet weak var selectedData: UILabel!
+    
+    var pickerData: [String] = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // assign the delegates
+        self.picker.delegate = self
+        self.picker.dataSource = self
+        
+        // initialise picker data
+        pickerData = ["A", "B", "C", "D", "E", "F"]
+    }
+    
+    // protocol UIPickerViewDataSource
+    // number of data components/columns
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // protocol UIPickerViewDataSource
+    // number of rows for a specified component/column
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    // protocol UIPickerViewDelegate
+    // setting the content/title of the corresponding row and component/column
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
     }
 
+    // protocol UIPickerViewDelegate
+    // responding to row selection
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectedData.text = pickerData[row]
+    }
 
 }
 
